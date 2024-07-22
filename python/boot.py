37de7249a10be5ac25479ff7df78877dcb605c86
Andrew <Skeeter4life1@gmail.com>
2024-07-21 19:10:31 -0700

    Addded display class, and made minor changes & fixes to clockstate.py and boot.py

---
+++ python/boot.py
@@ -0,0 +1,16 @@
+from clock_state import ClockState
+from Display import Oled
+import MenuSystem
+from push_button import PushButton
+from rotary_encoder import RotaryEncoder
+
+
+if __name__ == "__main__": #changed __name to __name__
+    encoder = RotaryEncoder(14,15,2)
+    accept_button = PushButton(13)
+    back_button = PushButton(16)
+    display = Oled(18,19,21,20,17)
+
+    state = ClockState()
+    menu_handler = menu.MenuHandler(encoder, accept_button, back_button, state)
+    
